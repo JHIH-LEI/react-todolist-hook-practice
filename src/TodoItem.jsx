c76e5ae5d9ce51525757b5a80da42d7224f233ae
Alicia <vicky88888@gmail.com>
2024-05-05 19:44:13 -0400

    refector: TodoItem component
    1) switch render logic refector (by isEdit)
    2) editTodo item manage by ref

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,66 +1,78 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { TodoContext } from "./TodoContext";
 import { useState } from "react";
 
 export function TodoItem({ todo }) {
   const [isEdit, setIsEdit] = useState(false);
+  const editTodoInput = useRef();
   const { toggleTodo, deleteTodo, editTodo } = useContext(TodoContext);
-  // form
   return (
     <li className="list-item">
-      <form
-        action=""
-        onSubmit={(e) => {
-          e.preventDefault();
-          console.log("提交");
-          if (isEdit) {
-            // 應該用ref不是這樣拿dom資料
-            editTodo(todo.id, e.target.editTodoInput.value);
-          }
-          setIsEdit(!isEdit);
-        }}
-      >
-        <label className="list-item-label">
-          <input
-            checked={todo.completed}
-            type="checkbox"
-            data-list-item-checkbox
-            onChange={(e) => toggleTodo(todo.id, e.target.checked)}
-          />
-
-          {isEdit ? (
+      {isEdit ? (
+        <form
+          action=""
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (isEdit) {
+              editTodo(todo.id, editTodoInput.current.value);
+            }
+            setIsEdit(!isEdit);
+          }}
+        >
+          <label className="list-item-label">
             <input
-              autoFocus
-              type="text"
-              name="editTodoInput"
-              defaultValue={todo.name}
+              checked={todo.completed}
+              type="checkbox"
+              data-list-item-checkbox
+              onChange={(e) => toggleTodo(todo.id, e.target.checked)}
             />
-          ) : (
-            <span data-list-item-text>{todo.name}</span>
-          )}
-        </label>
-        <button onClick={() => deleteTodo(todo.id)} data-button-delete>
-          Delete
-        </button>
 
-        {isEdit ? (
+            {isEdit ? (
+              <input
+                autoFocus
+                type="text"
+                name="editTodoInput"
+                defaultValue={todo.name}
+                ref={editTodoInput}
+              />
+            ) : (
+              <span data-list-item-text>{todo.name}</span>
+            )}
+          </label>
+          <button onClick={() => deleteTodo(todo.id)} data-button-delete>
+            Delete
+          </button>
+
           <button type="submit" data-button-delete>
             save
           </button>
-        ) : (
+        </form>
+      ) : (
+        <div>
+          <label className="list-item-label">
+            <input
+              checked={todo.completed}
+              type="checkbox"
+              data-list-item-checkbox
+              onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+            />
+            <span data-list-item-text>{todo.name}</span>
+          </label>
+          <button onClick={() => deleteTodo(todo.id)} data-button-delete>
+            Delete
+          </button>
+
           <button
             type="button"
-            onClick={(e) => {
-              // 避免表單提交
-              e.preventDefault();
-              setIsEdit(!isEdit);
+            onClick={() => {
+              setIsEdit(true);
             }}
             data-button-delete
           >
             edit
           </button>
-        )}
-      </form>
+        </div>
+      )}
     </li>
   );
 }
